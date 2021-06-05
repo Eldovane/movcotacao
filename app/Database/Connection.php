6b6924f5c74e3e47234b614ea12f70fe1d7ad394
Pedro Henrique Ribeiro Moreira <p3drohenrique.moreira@gmail.com>
2021-06-05 17:41:17 -0300

    Adicionando comentarios.

--- app/Database/Connection.php
+++ app/Database/Connection.php
@@ -6,10 +6,15 @@
 
   class Connection {
     public static function getConnection() {
+      // Importando a configuração do banco de dados do projeto.
       $databaseConfig = require __DIR__ . '/../../src/config/database.php';
 
+      // Selecionando qual conexão a aplicação irá trabalhar.
+      // Lembrar de trocar para produção quando for subir a aplicação.
+      // Por enquanto não sei como mudar issi dinâmicamente.
       $connectionProperties = $databaseConfig['testing'];
 
+      // Criando a instância do mysqli;
       $connection = new mysqli(
         $connectionProperties['host'],
         $connectionProperties['user'],
@@ -18,10 +23,12 @@
         $connectionProperties['port']
       );
 
+      // Verificando se houve algum erro ao tentar criar a instância do mysqli.
       if ($connection->connect_error) {
         throw new Exception('Houve um erro ao criar uma conexão com o banco de dados.');
       }
 
+      // Entregando a conexão criar no instânciamento do mysqli.
       return $connection;
     }
   }

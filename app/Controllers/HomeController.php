d0bf0ad74b7e6176cc41c2d901dc9a944b5a3adf
Pedro Henrique Ribeiro Moreira <p3drohenrique.moreira@gmail.com>
2021-06-07 18:08:00 -0300

    Deploy

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -14,7 +14,7 @@
     public function show(Request $request, Response $response): Response {
       if(isset($_COOKIE['@movcotacao:token'])) {
         $settings = $this->container->get('settings');
-        return $response->withHeader('Location', $settings['app_url'] . '/cotacao');
+        return $response->withHeader('Location', $settings['app_url'] . '/lista');
       }
 
       return $this->container->get('view')->render($response, 'index.html');
